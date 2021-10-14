17ca4880d1a3cfc3ab8fb91f33b08e4c3d431d93
dengweiwei <1412891697@qq.com>
2021-10-15 00:21:35 +0800

    Adjust Files

--- include/encodeAndDecode.h
+++ include/encodeAndDecode.h
@@ -9,6 +9,7 @@
 #include<cryptopp/osrng.h>
 #include<cryptopp/hex.h>
 #include<cryptopp/xed25519.h>
+#include<cryptopp/base64.h>
 #include<exception>
 using namespace CryptoPP;
 using std::cout;
@@ -17,90 +18,47 @@ using std::endl;
 using std::string;
 using std::cerr;
 using std::runtime_error;
-/*
+
 //PKCS#8 对私钥进行 Encode 和 Decode ,X.509对公钥进行 Encode 和 Decode
 //即实现对公私钥的 Save 和 Load(保存和加载)
-void Save(const string& filename, const BufferedTransformation& bt)
-{
-    FileSink file(filename.c_str());
+void Save(const string& filename, const BufferedTransformation& bt);
+void SavePublicKey(const string& filename, const PublicKey& key);
 
-    bt.CopyTo(file);
-    file.MessageEnd();
-}
+void Load(const string& filename, BufferedTransformation& bt);
+void LoadPublicKey(const string& filename, PublicKey& key);
 
-void SavePublicKey(const string& filename, const PublicKey& key)
-{
-    //HexEncoder encoder(new FileSink(filename.c_str()));
-    ByteQueue queue;
-    //HexEncoder encoder;
-    key.Save(queue);
-    Save(filename, queue);
-}
-*/
-/*
-void Load(const string& filename, BufferedTransformation& bt)
-{
-    FileSource file(filename.c_str(), true );//pumpAll/
 
-    file.TransferTo(bt);
-    bt.MessageEnd();
-}
 
-void LoadPublicKey(const string& filename, PublicKey& key)
-{
-    ByteQueue queue;
-    Load(filename, queue);
-
-    key.Load(queue);    
-}
-*/
 //使用 DEREncodePublicKey 和 DEREncodePrivateKey 实现对公私钥的 Encode (编码)
-void Encode(const string& filename, const BufferedTransformation& bt)
-{
-    FileSink file(filename.c_str());
+void Encode(const string& filename, const BufferedTransformation& bt);
+void EncodePrivateKey(const string& filename, const RSA::PrivateKey& key);
+void EncodePublicKey(const string& filename, const RSA::PublicKey& key);
 
-    bt.CopyTo(file);
-    file.MessageEnd();
-}
 
-void EncodePrivateKey(const string& filename, const RSA::PrivateKey& key)
-{
-    ByteQueue queue;
-    key.DEREncodePrivateKey(queue);
-
-    Encode(filename, queue);
-}
-
-void EncodePublicKey(const string& filename, const RSA::PublicKey& key)
-{
-    ByteQueue queue;
-    key.DEREncodePublicKey(queue);
-
-    Encode(filename, queue);
-}
 
 //使用 EBRDecodePublicKey 和 EBRDecodePrivateKey 实现对公私钥的 Decode (编码)
-void Decode(const string& filename, BufferedTransformation& bt)
-{
-    FileSource file(filename.c_str(), true /*pumpAll*/);
+void Decode(const string& filename, BufferedTransformation& bt);
+void DecodePrivateKey(const string& filename, RSA::PrivateKey& key);
+void DecodePublicKey(const string& filename, RSA::PublicKey& key);
 
-    file.TransferTo(bt);
-    bt.MessageEnd();
-}
-void DecodePrivateKey(const string& filename, RSA::PrivateKey& key)
-{
-    ByteQueue queue;
 
-    Decode(filename, queue);
-    key.BERDecodePrivateKey(queue, false /*paramsPresent*/, queue.MaxRetrievable());
-}
 
-void DecodePublicKey(const string& filename, RSA::PublicKey& key)
-{
-    ByteQueue queue;
+//使用 HexEncoder 和 HexDecoder 对公私钥进行编码(Encode/Decode)
+void SaveHexPrivateKey(const string& filename, const PrivateKey& key);
+void SaveHexPublicKey(const string& filename, const PublicKey& key);
+void SaveHex(const string& filename, const BufferedTransformation& bt);
 
-    Decode(filename, queue);
-    key.BERDecodePublicKey(queue, false /*paramsPresent*/, queue.MaxRetrievable());
-}
+void LoadHexPrivateKey(const string& filename,const PrivateKey& key);
+void LoadHexPublicKey(const string& filename,const PublicKey& key);
+void LoadHex(const string& filename,const BufferedTransformation& bt);
+
+//使用 Base64Encoder 和 Base64Decoder 对公私钥进行编码(Encode/Decode)
+void SaveBase64PrivateKey(const string& filename, const PrivateKey& key);
+void SaveBase64PublicKey(const string& filename, const PublicKey& key);
+void SaveBase64(const string& filename, const BufferedTransformation& bt);
+
+void LoadBase64PrivateKey(const string& filename,const PrivateKey& key);
+void LoadBase64PublicKey(const string& filename,const PublicKey& key);
+void LoadBase64(const string& filename,const BufferedTransformation& bt);
 
 #endif
